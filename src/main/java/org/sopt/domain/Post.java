c002378b64ffe5f1e8f37e5e00f1318f264a46c9
haerxeong <haeryeong04@naver.com>
2026-05-30 00:04:32 +0900

    feat: Post 엔티티에 컬럼, 테이블 어노테이션 적용

--- src/main/java/org/sopt/domain/Post.java
+++ src/main/java/org/sopt/domain/Post.java
@@ -1,12 +1,17 @@
 package org.sopt.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,22 +20,35 @@ import lombok.NoArgsConstructor;
 @Entity  // "이 클래스를 DB 테이블과 매핑해요" — 영속성 컨텍스트가 이 클래스를 관리해요
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+    indexes = {
+        @Index(name = "idx_post_board_type", columnList = "board_type")
+    }
+)
 public class Post extends BaseTimeEntity {
 
   @Id // 앞에서 배운 PK
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(nullable = false, length = 100)
   private String title;
 
+  @Column(nullable = false, columnDefinition = "TEXT")
   private String content;
 
   @ManyToOne(fetch = FetchType.LAZY)  // User : Post = 1 : N
   @JoinColumn(name = "user_id")       // post 테이블에 user_id FK 컬럼이 생겨요
   private User user;
 
+  @Column(nullable = false, name = "is_question")
   private boolean isQuestion;
+
+  @Column(nullable = false, name = "is_anonymous")
   private boolean isAnonymous;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, name = "board_type", length = 20)
   private BoardType boardType;
 
   @Builder
